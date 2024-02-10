095a91244f0660191f7ca7543479775976067d3d
Aditya <adi22feb@gmail.com>
2024-02-10 21:01:55 +0530

    Added description generation

--- src/components/text.tsx
+++ src/components/text.tsx
@@ -1,10 +1,35 @@
 // import React from 'react'
 
 // import Feature from './reusableFeatures';
+import getChatGPTResponse from '@/api/chatGPTResponse';
 import InfoBox from './reusableInfobox';
 import { Separator } from './ui/separator';
+import { useState } from 'react';
+
+
 
 export default function Text() {
+
+  const [prompt, setPrompt] = useState<string>("");
+  const [description,setDescription] = useState<string>("");
+  
+  const handleGenerateClick = async () => {
+  try {
+    console.log("clicked");
+    setDescription("Please wait..Loading!!!!!!")
+    const response = await getChatGPTResponse(prompt);
+    // console.log('Generated Response:', response);
+    setDescription(response);
+
+    // Handle the response as needed (e.g., update state in a React component)
+    // For example, using React state:
+    // setGeneratedResponse(response);
+  } catch (error) {
+    console.error('Error generating response:', error);
+    // Handle errors as needed
+  }
+};
+
   return (
     <div className="flex ">
       <div className="w-1/3 flex flex-col  h-full border-[#D4D4D4] rounded-b-lg mt-4 ">
@@ -22,6 +47,9 @@ export default function Text() {
             rows={5}
             className="mt-1 p-2 block w-full border border-gray-300 rounded-md focus:ring focus:ring-indigo-200 focus:outline-none focus:border-indigo-400 sm:text-sm"
             placeholder="Type your message here"
+            onChange={(e:any)=>{
+              setPrompt(e.target.value)
+            }}
           />
         </div>
         <Separator />
@@ -50,14 +78,14 @@ export default function Text() {
           >
             Next
           </button>
-          <button className="bg-[#623FC4] w-1/2 items-center justify-center font-semibold rounded-md cursor-pointer text-white">
+          <button className="bg-[#623FC4] w-1/2 items-center justify-center font-semibold rounded-md cursor-pointer text-white" onClick={handleGenerateClick}>
             Generate
           </button>
         </div>
       </div>
       <Separator orientation="vertical" className="" />
       <div className="w-2/3 bg-white p-8">
-        <div className="w-full h-[350px] rounded-md border border-[#623FC4]"></div>
+        <div className="w-full h-[350px] rounded-md border border-[#623FC4] p-4">{description}</div>
       </div>
     </div>
   );
