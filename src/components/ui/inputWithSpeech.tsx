1c3ad5dfa33aa67735b5662f9dce30fbe1c3a242
ShahIsCoding <cpabhishekshah0036@hotmail.com>
2024-02-10 11:55:05 +0530

    using a package

--- src/components/ui/inputWithSpeech.tsx
+++ src/components/ui/inputWithSpeech.tsx
@@ -1,6 +1,4 @@
-import useSpeechReco from '@/hooks/useSpeechReco';
-import { useEffect, useState } from 'react';
-
+import useSpeechToText from 'react-hook-speech-to-text';
 interface InputWithSpeech {
   inputValue: any;
   name: string;
@@ -15,8 +13,26 @@ const InputWithSpeech = ({
   inputValue,
   setInput,
 }: InputWithSpeech) => {
-  const { recognition, isListening, startListening, stopListening } =
-    useSpeechReco(setInput);
+  const {
+    error,
+    interimResult,
+    isRecording,
+    results,
+    startSpeechToText,
+    stopSpeechToText,
+  } = useSpeechToText({
+    continuous: false,
+    timeout: 3000,
+    useLegacyResults: false,
+  });
+  const handleStop = () => {
+    stopSpeechToText();
+    setInput(interimResult);
+  };
+  const handleStart = () => {
+    setInput('');
+    startSpeechToText();
+  };
   return (
     <>
       <label htmlFor={name} className="block font-bold text-[#000000]">
@@ -32,15 +48,15 @@ const InputWithSpeech = ({
           value={inputValue}
           onChange={e => setInput(e.target.value)}
         />
-        {recognition && (
+        {!error && (
           <div>
-            {isListening ? (
+            {isRecording ? (
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
                 className="w-6 h-6"
-                onClick={stopListening}
+                onClick={handleStop}
               >
                 <path d="M8.25 4.5a3.75 3.75 0 1 1 7.5 0v8.25a3.75 3.75 0 1 1-7.5 0V4.5Z" />
                 <path d="M6 10.5a.75.75 0 0 1 .75.75v1.5a5.25 5.25 0 1 0 10.5 0v-1.5a.75.75 0 0 1 1.5 0v1.5a6.751 6.751 0 0 1-6 6.709v2.291h3a.75.75 0 0 1 0 1.5h-7.5a.75.75 0 0 1 0-1.5h3v-2.291a6.751 6.751 0 0 1-6-6.709v-1.5A.75.75 0 0 1 6 10.5Z" />
@@ -53,7 +69,7 @@ const InputWithSpeech = ({
                 strokeWidth={1.5}
                 stroke="currentColor"
                 className="w-6 h-6"
-                onClick={startListening}
+                onClick={handleStart}
               >
                 <path
                   strokeLinecap="round"
